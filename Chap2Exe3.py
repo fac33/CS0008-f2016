daf0765035ce703db02a284f601ac2ebca0671c6
lenovo <fac33@pitt.edu>
2016-09-15 14:45:21 -0700

    exercise

---
+++ Chap2Exe3.py
@@ -0,0 +1,6 @@
+meter = float(\
+    input(\
+        "could you please type how many square meters do you have?"))
+arcs = meter/ 4046.8564224
+print(arcs)
+
---
+++ Chap2Exe5.py
@@ -0,0 +1,8 @@
+distance1 = 6*90
+distance2 = 10*90
+distance3 = 15*90
+distance4 = 2*90 + 90/4
+print("The distance the car will travel in 6 hours is", distance1, "KM")
+print("The distance the car will travel in 10 hours is", distance2, "KM")
+print("The distance the car will travel in 15 hours is", distance3, "KM")
+print("The distance the car will travel in 2 hours and 25 minutes is", distance4, "KM")
