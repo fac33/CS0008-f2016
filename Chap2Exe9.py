f063615c9ed996b3749fe0605ddc1b307934f36f
lenovo <fac33@pitt.edu>
2016-09-16 22:46:52 -0700

    New changelist

---
+++ Chap2Exe9.py
@@ -0,0 +1,16 @@
+# Template for code submission
+# name: Fanyang Cheng
+# email: fac33@pitt.edu
+# date: 09/06/2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+#
+# Description
+# description of this file goes here
+# Starting with Python, Chapter 1, Exercise 9
+# notes
+# template
+
+fde = float(input("please enter the temperature in Fahrenheit, thank you."))
+cde = 5 * ( fde-32 ) / 9
+print("the temperature in Celsius is ",cde)
