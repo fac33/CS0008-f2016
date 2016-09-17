d551073da6623114712acf4254b92cbf688e3f4d
lenovo <fac33@pitt.edu>
2016-09-16 21:24:11 -0700

    exercise

--- Chap2Exe3.py
+++ Chap2Exe3.py
@@ -1,3 +1,16 @@
+# Template for code submission
+# name: Fanyang Cheng
+# email: fac33@pitt.edu
+# date: 09/06/2016
+# class: CS0008-f2016
+# instructor: Max Novelli (man8@pitt.edu)
+#
+# Description
+# description of this file goes here
+# Starting with Python, Chapter 1, Exercise 3
+# notes
+# template
+
 meter = float(\
     input(\
         "could you please type how many square meters do you have?"))
