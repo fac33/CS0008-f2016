d1f4ee51e9ec39600dea8fd58a8aa6562deb9dfd
lenovo <fac33@pitt.edu>
2016-09-27 16:10:57 -0700

    New changelist

--- chap2/Chap2Exe3.py
+++ chap2/Chap2Exe3.py
@@ -11,9 +11,7 @@
 # notes
 # template
 
-meter = float(\
-    input(\
-        "could you please type how many square meters do you have?"))
+meter = float(input("could you please type how many square meters do you have?"))
 arcs = meter/ 4046.8564224
 print(arcs)
 
